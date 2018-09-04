39da2f3a8986bcd8a9db3d69400da94da421b605
slince <taosikai@yeah.net>
2018-09-04 18:04:33 +0800

    add

--- tests/Filesystem/TempTest.php
+++ tests/Filesystem/TempTest.php
@@ -3,10 +3,10 @@
 namespace Slince\Upload\Tests\Filesystem;
 
 use PHPUnit\Framework\TestCase;
-use Slince\Upload\Filesystem\Local;
+use Slince\Upload\Filesystem\Temp;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 
-class LocalTest extends TestCase
+class TempTest extends TestCase
 {
     public function testUpload()
     {
@@ -20,8 +20,8 @@ class LocalTest extends TestCase
             null,
             true
         );
-        $local = new Local(__DIR__ . '/../Fixtures/dst/');
-        $file = $local->upload('hello-2.txt', $file);
-        $this->assertContains(sys_get_temp_dir(), $file->getPathname());
+
+        $temp = new Temp();
+        $temp->upload('hello-3.txt', $file);
     }
 }
